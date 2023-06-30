19bc0facca1e72623e9cfe7c08688ebfcf06648c
aliruman <aliruman9@gmail.com>
2023-06-30 17:22:55 +0530

    Commit made from VS Code Azure Static Web Apps

---
+++ src/settings/appsettings.ts
@@ -0,0 +1,6 @@
+// App settings for the agent-portal
+export const appsettings = {
+    agentHubBaseAddress: 'https://www.c-sharpcorner.com/UploadFile/puranindia/sql-server-interview-questions/',//'http://localhost:7071',
+    webPusSubConnectionString: 'Endpoint=https://myazcommunicationatwps.webpubsub.azure.com;AccessKey=/qjyQKnW/LSMFedcwaq1TvMXICdPHGfr/FaTZ4b9gvA=;Version=1.0;',
+    webPubSubHubName: 'refreshConversations'
+};
